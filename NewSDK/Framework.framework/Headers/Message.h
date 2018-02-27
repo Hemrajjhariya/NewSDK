dea6ee62b3048dce3117f231add88887093e8eac
Hemraj <hemrajjhariya@mail.com>
2018-02-27 11:26:10 +0530

    add uiclass.

---
+++ NewSDK/Framework.framework/Headers/Message.h
@@ -0,0 +1,15 @@
+//
+//  Message.h
+//  Framework
+//
+//  Created by Daljeet Singh on 26/02/18.
+//  Copyright © 2018 Daljeet Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Message : UIViewController
+
++(void)printMessage:(NSString *)message;
+
+@end
